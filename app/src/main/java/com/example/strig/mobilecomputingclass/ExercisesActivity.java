419486891c32b4b5ad9bf2617fac2de36d616a99
Anuar Otynshin <strigeandro@gmail.com>
2018-03-29 16:32:43 +0600

    Add 16/01/18 lecture exercise (Dragging)

--- app/src/main/java/com/example/strig/mobilecomputingclass/ExercisesActivity.java
+++ app/src/main/java/com/example/strig/mobilecomputingclass/ExercisesActivity.java
@@ -19,7 +19,7 @@ public class ExercisesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
 
-        exercisesList = (ListView) findViewById(R.id.exercises_list);
+        exercisesList = findViewById(R.id.exercises_list);
 
         AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
             @Override
@@ -27,6 +27,51 @@ public class ExercisesActivity extends AppCompatActivity {
                 if (i == 0) {
                     intent = new Intent(ExercisesActivity.this, TouchAndViewActivity.class);
                     startActivity(intent);
+                } else if (i == 1) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 2) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 3) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 4) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 5) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 6) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 7) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 8) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 9) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 10) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 11) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 12) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 13) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 14) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
+                } else if (i == 15) {
+//                    intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
+//                    startActivity(intent);
                 } else if (i == 16) {
                     intent = new Intent(ExercisesActivity.this, LiveWallpaperActivity.class);
                     startActivity(intent);
